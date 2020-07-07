fea5e7975d7fb5d09ff2059cba06196147ca8cbe
Ian Hutson <ih00209@georgiasouthern.edu>
2020-07-07 14:35:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,22 @@
 class Student
-
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+ 
+  attr_accessor :name, :grade
+  attr_reader :id
+  def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+ 
+  def self.create_table
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
+    DB[:conn].execute(sql) 
+  end
+ 
 end
